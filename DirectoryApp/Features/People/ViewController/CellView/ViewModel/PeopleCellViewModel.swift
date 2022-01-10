d8ad985585f6b0d3bfc8ee71a8dc7efb617e56a3
oluwatobiHammed <oladipupo_tobi@yahoo.com>
2022-01-10 09:18:12 +0000

    first commit

---
+++ DirectoryApp/Features/People/ViewController/CellView/ViewModel/PeopleCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PeopleTableViewCellViewModel.swift
+//  DirectoryApp
+//
+//  Created by Oladipupo Oluwatobi on 07/01/2022.
+//
+
+import Foundation
+
+
+struct PeopleTableViewCellViewModel {
+    
+}
